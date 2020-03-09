f0bf255d28d78ee11c0f0af53ac0188de00b3be2
dragon-web <1582943265@qq.com>
2020-03-09 11:42:36 +0800

    类和对象深入了解

---
+++ Cpp_review_2/Cpp_review_2/class.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+/*
+class Date
+{
+private:
+	int d, m, y;
+public:
+	int day()const
+	{
+		return d;
+	}
+	int year()const
+	{
+		return y;
+	}
+	int month()const
+	{
+		return m;
+	}
+	Date (int day, int month, int year);
+	void print();
+};
+
+Date::Date (int day,int month,int year)
+{
+	 d = day;
+	 m = month;
+	 y = year;
+}
+void Date::print()
+{
+	cout << y << "Äê" << m << "ÔÂ" << d << "ÈÕ";
+}
+
+int main()
+{
+	Date data1(1,10,1949);
+
+	data1.print();
+	system("pause");
+	return 0;
+}
+*/
+
+
+
+int main()
+{
+	
+	system("pause");
+	return 0; 
+}
