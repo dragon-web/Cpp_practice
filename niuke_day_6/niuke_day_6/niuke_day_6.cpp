97f85a27db13df66881b3bf7bc2be56de81edb61
dragon-web <1582943265@qq.com>
2020-03-18 11:01:21 +0800

    niuke_day_6

---
+++ niuke_day_6/niuke_day_6/niuke_day_6.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+/*
+int main()
+{
+	int num = 0;
+	while (cin >> num)
+	{
+		int count = 0;
+		while (num)
+		{
+			num = num & (num - 1);
+			count++;
+		}
+		cout << count << endl;
+	}
+	system("pause");
+	return 0;
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num = 0;
+	cin >> num;
+	int count = 0;
+	while (num)
+	{
+		num = num & (num - 1);
+		count++;
+	}
+	cout << count << endl;
+	system("pause");
+	return 0;
+}
+*/
