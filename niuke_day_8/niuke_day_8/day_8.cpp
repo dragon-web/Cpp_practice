d39c06d0f1f0e76753ebbc2ca9bb96d08be559ee
dragon-web <1582943265@qq.com>
2020-03-21 20:25:43 +0800

    niuke_day_8

---
+++ niuke_day_8/niuke_day_8/day_8.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//int a[][3] = { {0,,2},{},{3,4,5} };一个逗号可以，但是两个逗号不行
+	string s1, s2;
+
+
+	system("pause");
+	return 0;
+}
