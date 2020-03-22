fdd57ef7eafd5c1a519c6121c87ebea1919c24cd
dragon-web <1582943265@qq.com>
2020-03-22 15:02:14 +0800

    niuke_day_8

--- niuke_day_8/niuke_day_8/day_8.cpp
+++ niuke_day_8/niuke_day_8/day_8.cpp
@@ -7,8 +7,8 @@ int main()
 {
 	//int a[][3] = { {0,,2},{},{3,4,5} };一个逗号可以，但是两个逗号不行
 	//string s1, s2;
-	int arr[][4] = { {1},{3,2},{4,5,6},{0} };
-	cout << arr[1][2] << endl;
+	//int arr[][4] = { {1},{3,2},{4,5,6},{0} };
+	//cout << arr[1][2] << endl;
 	system("pause");
 	return 0;
 }
