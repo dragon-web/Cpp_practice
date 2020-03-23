6da415925c7f6908d26281059349fcd1ccaf68cf
dragon-web <1582943265@qq.com>
2020-03-23 23:56:38 +0800

    回文数的判断

--- niuke_day_8/niuke_day_8/day_8.cpp
+++ niuke_day_8/niuke_day_8/day_8.cpp
@@ -2,6 +2,47 @@
 #include<iostream>
 
 using namespace std;
+//判断一个数是否是回文数
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		if (x < 0)
+			return false;
+		else
+		{
+			int count = 1;
+			int temp1 = x;
+			while (temp1/10)
+			{
+				temp1 /= 10;
+				count++;
+			}
+			int count1 = count;
+			int *p = (int*)malloc(sizeof(int)*count);
+			int temp = 0;
+			while (x)
+			{
+				temp = x % 10;
+				x /= 10;
+				p[count - 1] = temp;
+					count--;
+			}
+			int first = 0, last = count1 - 1;
+			while (first <= last)
+			{
+				if (p[first] == p[last])
+				{
+					first++;
+					last--;
+				}
+				else
+					return false;
+			}
+			return true;
+		}
+		
+	}
+};
 
 int main()
 {
@@ -9,6 +50,15 @@ int main()
 	//string s1, s2;
 	//int arr[][4] = { {1},{3,2},{4,5,6},{0} };
 	//cout << arr[1][2] << endl;
+	Solution a;
+	int num = 11;
+	if (a.isPalindrome(num) == true)
+		cout << "回文数" << endl;
+	else
+	{
+		cout << "不是回文数" << endl;
+	}
+
 	system("pause");
 	return 0;
 }
