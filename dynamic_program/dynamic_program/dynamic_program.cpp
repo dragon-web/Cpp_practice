af54b9106d4d43c52ab04ab80ad2d76760f0b025
dragon-web <1582943265@qq.com>
2020-03-27 12:25:31 +0800

    C语言实现字符串匹配问题

--- dynamic_program/dynamic_program/dynamic_program.cpp
+++ dynamic_program/dynamic_program/dynamic_program.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include<stdio.h>
 #include<Windows.h>
+#include<string.h>
 /*
 int f(int i, int j, int(*a)[4])
 {
@@ -28,16 +29,71 @@ int main()
 	return 0;
 }
 */
-
-void printcom(char** a)
+//拿短的去匹配长的
+/*
+void printcom(char* arr1,char*arr2)
 {
+	int i = 0;
+	int j = 0;
+	int temp = 0;
+	int temp1 = 0;
+	size_t length1 = strlen(arr1);
+	size_t length2 = strlen(arr2);
+	for (i = 0; i < length1; ++i)
+	{
+		for (j = 0; j < length2; ++j)
+		{
+			if (arr2[j] == arr1[i])
+			{
+				j++;
+				i++;
+				temp++;
+			}
+//判断连续与否
+		}
 
-
+	}
+}
+*/
+void printcom(char* arr1, char*arr2)
+{
+	size_t length1 = strlen(arr1);
+	size_t length2 = strlen(arr2);
+	int temp = 0;
+	int** p = (int**)malloc(length1*sizeof(int));
+	for (int k = 0; k < length1; ++k)
+		p[k] = (int*)malloc(sizeof(int)*length2);
+	for (int i = 0; i < length1; ++i)
+	{
+		for (int j = 0; j < length2; ++j)
+		{
+			p[i][j] = 0;
+			if (arr1[i] == arr2[j])
+			{
+				p[i][j] = 1;
+				if (i > 0 && j > 0)
+				{
+					p[i][j] += p[i - 1][j - 1];
+				}
+				
+			}
+			if (p[i][j] > temp)
+			{
+				temp = p[i][j];
+			}
+		}
+	}
+	printf("%d", temp);
 }
 int main()
 {
-  
+	char arr1[100];
+	char arr2[100];
+	scanf("%s", arr1);
+	scanf("%s", arr2);
+	printcom(arr1, arr2);
 
 	system("pause");
 	return 0;
 }
+//char** q = (char**)malloc(length1*length2);
