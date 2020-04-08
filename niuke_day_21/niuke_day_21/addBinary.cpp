e30b49b3ca00fe37d2a072b57abfbce80156b168
dragon-web <1582943265@qq.com>
2020-04-08 21:07:34 +0800

    AddBinary

---
+++ niuke_day_21/niuke_day_21/addBinary.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+string AddBinary(string &a, string &b)
+{
+		string res;
+		int carry = 0;         //表示进位
+		int x, y;
+		int i = a.size() - 1, j = b.size() - 1;
+		for (; i >= 0 || j >= 0; i--, j--) {
+			x = i >= 0 ? a[i] - '0' : 0;
+			y = j >= 0 ? b[j] - '0' : 0;
+			res += (x + y + carry) % 2 + '0';
+			carry = (x + y + carry) / 2;
+		}
+		if (carry)   res += '1';
+		reverse(res.begin(), res.end());
+		return res;
+}
+
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+	string res = AddBinary(a, b);
+	cout << res << endl;
+	system("pause");
+	return 0;
+}
