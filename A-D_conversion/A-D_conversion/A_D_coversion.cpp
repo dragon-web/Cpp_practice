829c0b30d0e5310fe83f02dc57d9c16944907a11
dragon-web <1582943265@qq.com>
2020-04-13 11:15:38 +0800

    AD转换失败品

---
+++ A-D_conversion/A-D_conversion/A_D_coversion.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<iostream>
+
+using namespace std;
+
+/*class Coversion
+{
+private:
+	int m_data;
+public:
+	void paintRow();
+	void paintLine();
+};
+void Coversion::paintLine()
+{
+	for (double a = 0; a < 0.2; a += 0.01)
+	{
+		cout << "|";
+		if (a + 0.01 == 0.2)
+			break;
+		else
+			cout << endl;
+	}
+}
+void Coversion::paintRow()
+{
+	cout << " ";
+	for (double i = 0.00; i < 0.8; i += 0.01)
+	{
+		cout << "_";
+	}
+	cout << endl;
+	for (double i = 0.00; i < 0.8; i += 0.01)
+	{
+		cout << " ";
+	}
+	cout << "/";
+}
+int main()
+{
+		Coversion a;
+	a.paintLine();
+	a.paintRow();
+	int length = 80;
+	for(int i = 0; i < length - 1; i++)
+	{
+	int x1 = a[i];int y1 = b[i];
+	int x2 = a[i + 1]; int y2 = b[i + 1];
+	line(x1,y1,x2,y2);
+	}
+	system("pause");
+	return 0;
+}*/
+/*#include <stdio.h>
+#include<stdlib.h>
+int main() {
+	for (float y = 1.5f; y > -1.5f; y -= 0.1f) {
+		for (float x = -1.5f; x < 1.5f; x += 0.05f) {
+			float z = x * x + y * y - 1;
+			float f = z * z * z - x * x * y * y * y;
+			putchar(f <= 0.0f ? ".:-=+*#%@"[(int)(f * -8.0f)] : ' ');
+		}
+		putchar('\n');
+	}
+	system("pause");
+	return 0;
+}
+*/
