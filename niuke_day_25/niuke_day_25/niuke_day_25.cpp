498629bc6562706dcbf95eabddfd730cbd2b868e
dragon-web <1582943265@qq.com>
2020-04-15 22:46:01 +0800

    niuke_day_25

--- niuke_day_25/niuke_day_25/niuke_day_25.cpp
+++ niuke_day_25/niuke_day_25/niuke_day_25.cpp
@@ -4,11 +4,14 @@
 using namespace std;
 int main()
 {
-	long a = (long)(((int*)0) + 4);
+	/*long a = (long)(((int*)0) + 4);
 	cout << a << endl;
 	char c = 'c';
 	int i = (int)c;
-	cout << i << endl;
+	cout << i << endl;*/
+	int i = 1;
+	printf("%d %d", ++i, ++i);
+	printf("%d %d", ++i, i++);
 	system("pause");
 	return 0;
 }
