58b46c7c72d08f575ecc655755e054adef9d8b6e
dragon-web <1582943265@qq.com>
2020-04-15 19:43:05 +0800

    niuke_day_25

---
+++ niuke_day_25/niuke_day_25/niuke_day_25.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNING
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	long a = (long)(((int*)0) + 4);
+	cout << a << endl;
+	char c = 'c';
+	int i = (int)c;
+	cout << i << endl;
+	system("pause");
+	return 0;
+}
