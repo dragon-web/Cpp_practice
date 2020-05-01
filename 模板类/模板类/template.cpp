66e2a1494b7f9f12867c246e476b9867ba60afdb
dragon-web <1582943265@qq.com>
2020-05-01 21:23:21 +0800

    模板类

---
+++ 模板类/模板类/template.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<iostream>
+#include<vector>
+#include<priority_queue>
+using namespace std;
+
+//函数模板可以重载
+
+//数组类模板
+/*
+template <typename T, int I>
+
+class CmyArry
+{
+private:
+	T data[I];
+public:
+	int SetItem(int index, const T & item);
+	int GetItem(int index, T & item);
+};
+
+template <class T>
+
+class Stack
+{
+private:
+	T* data;
+	int size;
+	int top;
+	int IsEmpty()
+	{
+		return top <= 0 ? 1 : 0;
+	}
+	int IsFull()
+	{
+		return top >= size ? 1 : 0;
+	}
+public:
+	Stack(int n)
+	{
+		date = new T[n];
+		size = n;
+		top = 0;
+	}
+	~Stack()
+	{
+		delete date;
+	}
+	void pop(T a);
+	T push();
+};
+
+template<typename T>
+
+T Stack<T>::push(T a)
+{
+
+
+
+
+}
+*/
+int main()
+{
+	system("pause");
+	return 0;
+}
