c73b901aeb2bbdfca95c63491d9ac8e7c4fc9f96
dragon-web <1582943265@qq.com>
2020-05-16 21:51:26 +0800

    哈夫曼树

---
+++ Huffman_Tree/Huffman_Tree/Huffman_Tree.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+//哈夫曼数主要用于编码和压缩
+
+#include<iostream>
+
+//哈夫曼编码 一般的实现时邻接表或者邻接矩阵
+
+typedef struct HuffNode
+{
+	int weigit;
+	int lchild;
+	int rchild;
+	int parents;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}
