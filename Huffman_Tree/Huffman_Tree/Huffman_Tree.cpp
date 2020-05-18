2444ff2529b6d4f7f18226a166e05e74494b7420
dragon-web <1582943265@qq.com>
2020-05-18 23:11:18 +0800

    HuffmanTree

--- Huffman_Tree/Huffman_Tree/Huffman_Tree.cpp
+++ Huffman_Tree/Huffman_Tree/Huffman_Tree.cpp
@@ -67,42 +67,79 @@ void Select(HuffNode huffTree[], int k, int& i1, int& i2)
 	i2 = temp2;
 }
 //a b c d 出现频率是 2 4 5 3 求哈哈夫曼编码
-void huffmanCoding()
 
-
-
-
-
-
-
-
-
-
-
+//具体体现为 对于 a b c d进行编码 然后给出编码情况
+void huffmanCoding(char* HuffCode[],HuffNode huffTree[],int n)//这个n是哈夫曼编码的个数  
+{
+	char* temp = new char[n];
+	temp[n - 1] = '\0';
+	for (int i = 0; i < n; ++i)//为啥是n而不是 2*n-1呢?因为我们只需要看叶子节点就行，没必要把所有的都遍历一遍
+	{
+		int start = n - 1;
+		int pos = i;
+		int parent = huffTree[i].parent;
+		while (parent != -1)
+		{
+			if (huffTree[parent].lchild == pos)
+				temp[--start] = '0';
+			else
+				temp[--start] = '1';
+			pos = parent;
+			parent = huffTree[parent].parent;
+		}
+		HuffCode[i] = new char[n - start];//这是个指针数组
+		strcpy(HuffCode[i], &temp[start]);
+	}
+	delete temp;
+}
+/*
 int main()
 {
-	int num = 5;
-	int arr[] = { 1,2,5,3,7 };
-	HuffNode huff[9];
+	int num = 3;
+	int arr[] = { 1,2,5};
+	HuffNode huff[5];
 	HuffmanTree(huff, arr, num);
-	for (int i = 0; i < 9; ++i)
+	for (int i = 0; i < 5; ++i)
 	{
 		cout << huff[i].weigit << " " << huff[i].parent << " " << huff[i].lchild << " " << huff[i].rchild;
 		cout << endl;
 	}
+	system("pause");
+	return 0;
+}
+*/
 
+
+
+
+
+
+//哈夫曼树的构建没有问题
+
+/*int main()
+{
+	//A B C的出现次数是1 2 5
+	int num = 3;
+	int arr[] = { 1,2,5};
+	HuffNode huff[5];
+	HuffmanTree(huff, arr, num);
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << huff[i].weigit << " " << huff[i].parent << " " << huff[i].lchild << " " << huff[i].rchild;
+		cout << endl;
+	}
+	cout << "______________________________________________________________________________" << endl;
+	char* code[3];
+	huffmanCoding(code, huff, num);
+	for (int i = 0; i < 3; ++i)
+	{
+		printf("%s\n", code[i]);
+	}
 	system("pause");
 	return 0;
 }
 
-
-
-
-
-
-
-
-
+*/
 
 /*
 int temp;
@@ -184,3 +221,12 @@ int main()
 	return 0;
 }
 */
+
+int main()
+{
+	int a = 1 && 2 && 3||4;
+	cout << a << endl;
+
+	system("pause");
+	return 0;
+}
