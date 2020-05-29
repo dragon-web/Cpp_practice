489e985d794a0ed8ead45acf0543ec1856de0d4a
dragon-web <1582943265@qq.com>
2020-05-29 22:44:50 +0800

    单项循环链表

---
+++ day__5_29/day__5_29/test_5_29.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	/*int arr[10] = { 0 };
+	int max = arr[0];
+	for (int i = 0; i < 10; ++i)
+	{
+		scanf("%d", &arr[i]);
+		if (arr[i] > max)
+		max = arr[i];
+	}
+	printf("%d\n", max);
+
+
+	int count = 1;
+	int num = 0;
+	int max = num;  
+	while (scanf("%d", &num),count <= 10)
+	{
+		max = max > num ? max : num;
+		count++;
+	}
+	printf("%d", max);
+	system("pause");
+	return 0;
+}
+*/
+
+
+
