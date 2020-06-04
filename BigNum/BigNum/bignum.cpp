01b7883a8b47ff273ce4a205a40b534365e28a61
dragon-web <1582943265@qq.com>
2020-06-04 16:38:25 +0800

    day_6_4

---
+++ BigNum/BigNum/bignum.cpp
@@ -0,0 +1,127 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*class A
+{
+public:
+	A()
+	{
+
+	}
+	void setName(const string& s) 
+	{
+		ss = s.c_str();
+		cout << &ss << endl;
+	}
+	void getss()
+	{
+		cout << *ss << endl;
+	}
+private:
+	const char* ss;
+};
+
+int main()
+{
+	/*A a1;
+	a1.setName("anbcd");
+	//cout << &a1.ss << endl;
+	a1.getss();
+	string a("abcd");
+	decltype(a.c_str());
+	system("pause");
+	return 0;
+}
+
+class A
+{
+private:
+	char *s;
+public:
+	void Solution(string &ori)
+	{
+		s = (char*)ori.c_str();
+	}
+	void cout_s()
+	{
+		cout << s << endl;
+	}
+};
+
+int main()
+{
+	string s;
+	cin >> s;
+	A a;
+	a.Solution(s);
+	a.cout_s();
+
+	system("pause");
+	return 0;
+}
+
+
+
+class A
+{
+public:
+	A(const string& s)
+	{
+		const char *p = s.c_str();
+		cout << &p << endl;
+		ss = (char*)p;
+	}
+void setName()
+	{
+		
+	}
+	void getss()
+	{
+		cout << ss << endl;
+	}
+private:
+	char* ss;
+};
+
+int main()
+{
+	A a1("anbcd");
+	a1.getss();
+	system("pause");
+	return 0;
+}*/
+
+ #include<iostream>
+#include<string>
+ using namespace std;
+
+ class A
+ {
+   public:
+	       A()
+		       {
+		
+		       }
+	       void setName(const string& s) //const 
+		         {
+		               ss = s.c_str();
+		                   cout << &ss << endl;
+		
+			         }
+	            const char* ss;
+	
+		 };
+
+ int main()
+ {
+	     A a1;
+	     a1.setName("anbcd");
+	     cout << &a1.ss << endl;
+	     cout << a1.ss << endl;
+		 system("pause");
+	     return 0;
+ }
+
