37ede31146878ab59051340c7d16f1f11552314e
dragon-web <1582943265@qq.com>
2020-06-30 22:02:20 +0800

    map

---
+++ Map/Map/Map.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<iostream>
+#include<string>
+#include<map>
+
+int main()
+{
+	pair<int, string> pv = { 1,"helloworld" };
+
+	system("pause");
+	return 0;
+}
